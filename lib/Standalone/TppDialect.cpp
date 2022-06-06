c05f1f050dab6c54b8ed5398b8c218d2422fcc18
lorenzo chelini <l.chelini@icloud.com>
2022-06-06 10:48:13 +0200

    rename files

--- lib/Standalone/TppDialect.cpp
+++ lib/Standalone/TppDialect.cpp
@@ -1,4 +1,4 @@
-//===- StandaloneDialect.cpp - Standalone dialect ---------------*- C++ -*-===//
+//===- TppDialect.cpp - Tpp dialect ----------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- lib/Standalone/TppOps.cpp
+++ lib/Standalone/TppOps.cpp
@@ -1,4 +1,4 @@
-//===- StandaloneOps.cpp - Standalone dialect ops ---------------*- C++ -*-===//
+//===- TppOps.cpp - Tpp dialect ops ----------------------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
