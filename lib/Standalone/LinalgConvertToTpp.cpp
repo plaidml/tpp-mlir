f8fb341d23819eadd0ff20dafe29025aef26616f
lorenzo chelini <l.chelini@icloud.com>
2022-06-09 11:29:36 +0200

    Re-think conversion to tpp

--- lib/Standalone/LinalgConvertToTpp.cpp
+++ lib/Standalone/LinalgConvertToTpp.cpp
@@ -11,6 +11,9 @@
 #include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Linalg/Transforms/Transforms.h"
+#include "mlir/Dialect/Linalg/Utils/Utils.h"
+#include "mlir/Interfaces/ViewLikeInterface.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 using namespace mlir;
@@ -24,49 +27,157 @@ using namespace mlir::tpp;
 
 namespace {
 
+// TODO: evaluate transform dialect.
+
+static SmallVector<Value, 4> getTileSizes(OpBuilder &builder,
+                                          linalg::LinalgOp linalgOp) {
+  SmallVector<Value, 4> tppTiles;
+  SmallVector<Range, 4> loopRanges =
+      linalgOp.createLoopRanges(builder, linalgOp.getLoc());
+  for (size_t i = 0; i < loopRanges.size(); i++)
+    tppTiles.push_back(loopRanges[i].size);
+  Value zeroVal = builder.create<arith::ConstantIndexOp>(linalgOp.getLoc(), 1);
+  tppTiles[0] = zeroVal;
+  tppTiles[1] = zeroVal;
+  return tppTiles;
+}
+
+static MemRefType dropUnitDims(MemRefType inputType, ArrayRef<int64_t> offsets,
+                               ArrayRef<int64_t> sizes,
+                               ArrayRef<int64_t> strides) {
+  Type rankReducedType = memref::SubViewOp::inferRankReducedResultType(
+      0, inputType, offsets, sizes, strides);
+  return canonicalizeStridedLayout(rankReducedType.cast<MemRefType>());
+}
+
+static Value rankReducingSubviewDroppingUnitDims(OpBuilder &builder,
+                                                 Location loc, Value input) {
+  MemRefType inputType = input.getType().cast<MemRefType>();
+  assert(inputType.hasStaticShape());
+  SmallVector<int64_t> subViewOffsets(inputType.getRank(), 0);
+  SmallVector<int64_t> subViewStrides(inputType.getRank(), 1);
+  ArrayRef<int64_t> subViewSizes = inputType.getShape();
+  MemRefType resultType =
+      dropUnitDims(inputType, subViewOffsets, subViewSizes, subViewStrides);
+  if (canonicalizeStridedLayout(resultType) ==
+      canonicalizeStridedLayout(inputType))
+    return input;
+  return builder.create<memref::SubViewOp>(
+      loc, resultType, input, subViewOffsets, subViewSizes, subViewStrides);
+}
+
+LogicalResult replaceGenericOpWithTppOp(linalg::GenericOp linalgOp) {
+  if (!linalgOp.hasBufferSemantics())
+    return linalgOp->emitError("Expect linalgOp with buffer semantics");
+
+  // bail-out if we don't need to do tiling or all the dimensions
+  // are not parallel.
+  // TODO: restrict to only the tiling ones.
+  if (linalgOp.getNumLoops() <= 2)
+    return success();
+  ArrayAttr iteratorTypes = linalgOp.iterator_types();
+  if (!llvm::all_of(iteratorTypes,
+                    [](Attribute type) { return isParallelIterator(type); }))
+    return success();
+
+  OpBuilder builder(linalgOp);
+  OpBuilder::InsertionGuard guard(builder);
+  linalg::LinalgTilingOptions linalgTilingOptions;
+  linalgTilingOptions.setLoopType(linalg::LinalgTilingLoopType::ParallelLoops)
+      .setTileSizeComputationFunction(getTileSizes);
+  IRRewriter rewriter(builder);
+  FailureOr<linalg::TiledLinalgOp> tiledOp =
+      linalg::tileLinalgOp(rewriter, linalgOp, linalgTilingOptions);
+  if (failed(tiledOp))
+    return linalgOp->emitError("Failed to tile linalgOp");
+
+  linalgOp->erase();
+  return success();
+}
+
 struct ConvertGenericOpToTpp : public OpRewritePattern<linalg::GenericOp> {
   using OpRewritePattern<linalg::GenericOp>::OpRewritePattern;
+
+  bool isTppCallPrefix(std::string stringAttrAsStr) const {
+    std::string delimiter = ".";
+    std::string prefix =
+        stringAttrAsStr.substr(0, stringAttrAsStr.find(delimiter));
+    return prefix.compare("tpp") == 0;
+  }
+
+  LogicalResult rewriteToTppOp(linalg::GenericOp linalgOp,
+                               ArrayRef<Value> operands,
+                               PatternRewriter &rewriter) const {
+    std::string libraryCall = linalgOp.getLibraryCallName();
+    // TODO: find better way to express this.
+    if (libraryCall.compare("tpp.identity") == 0) {
+      rewriter.replaceOpWithNewOp<tpp::IdentityOp>(linalgOp, operands[0],
+                                                   operands[1]);
+      return success();
+    }
+    if (libraryCall.compare("tpp.relu") == 0) {
+      rewriter.replaceOpWithNewOp<tpp::ReluOp>(linalgOp, operands[0],
+                                               operands[1]);
+      return success();
+    }
+    if (libraryCall.compare("tpp.add") == 0) {
+      rewriter.replaceOpWithNewOp<tpp::AddOp>(linalgOp, operands[0],
+                                              operands[1], operands[2]);
+      return success();
+    }
+    if (libraryCall.compare("tpp.matmul") == 0) {
+      rewriter.replaceOpWithNewOp<tpp::MatmulOp>(linalgOp, operands[0],
+                                                 operands[1], operands[2]);
+      return success();
+    }
+    return failure();
+  }
 
   LogicalResult matchAndRewrite(linalg::GenericOp linalgOp,
                                 PatternRewriter &rewriter) const override {
     if (!linalgOp.hasBufferSemantics())
       return failure();
-    std::string libraryCall = linalgOp.getLibraryCallName();
-    // TODO: find better way to express this.
-    if (libraryCall.compare("tpp.identity") == 0) {
-      rewriter.replaceOpWithNewOp<tpp::IdentityOp>(
-          linalgOp, linalgOp->getOperand(0), linalgOp->getOperand(1));
-      return success();
+
+    if (!linalgOp.library_callAttr() || linalgOp.getNumLoops() != 2 ||
+        !isTppCallPrefix(linalgOp.getLibraryCallName()))
+      return failure();
+
+    if (linalgOp->getNumResults() != 0)
+      return failure();
+
+    Location loc = linalgOp.getLoc();
+    SmallVector<Value, 4> newOperands;
+    for (Value operand : linalgOp->getOperands()) {
+      ShapedType operandType = operand.getType().cast<ShapedType>();
+      if (operandType.getRank() == 2) {
+        newOperands.push_back(operand);
+        continue;
+      }
+      if (operandType.getRank() > 2)
+        newOperands.push_back(
+            rankReducingSubviewDroppingUnitDims(rewriter, loc, operand));
+      else
+        assert(0 && "not implemented yet");
     }
-    if (libraryCall.compare("tpp.relu") == 0) {
-      rewriter.replaceOpWithNewOp<tpp::ReluOp>(
-          linalgOp, linalgOp->getOperand(0), linalgOp->getOperand(1));
-      return success();
-    }
-    if (libraryCall.compare("tpp.add") == 0) {
-      rewriter.replaceOpWithNewOp<tpp::AddOp>(linalgOp, linalgOp->getOperand(0),
-                                              linalgOp->getOperand(1),
-                                              linalgOp->getOperand(2));
-      return success();
-    }
-    if (libraryCall.compare("tpp.matmul") == 0) {
-      rewriter.replaceOpWithNewOp<tpp::MatmulOp>(
-          linalgOp, linalgOp->getOperand(0), linalgOp->getOperand(1),
-          linalgOp->getOperand(2));
-      return success();
-    }
-    return failure();
+    return rewriteToTppOp(linalgOp, newOperands, rewriter);
   }
 };
 
-void populateLinalgToTppPatterns(RewritePatternSet &patterns) {
+void populateConvertLinalgToTppPatterns(RewritePatternSet &patterns) {
   patterns.add<ConvertGenericOpToTpp>(patterns.getContext());
 }
 
+// TODO: PatternRwriter does not work well with tiling. I suspect
+// because the builder is not properly propagated. But investigate more.
 struct ConvertLinalgToTpp : public ConvertLinalgToTppBase<ConvertLinalgToTpp> {
   void runOnOperation() override {
-    RewritePatternSet patterns(&getContext());
-    populateLinalgToTppPatterns(patterns);
+    getOperation().walk([&](linalg::GenericOp linalgOp) {
+      // TODO: Check logical result.
+      (void)replaceGenericOpWithTppOp(linalgOp);
+    });
+    RewritePatternSet patterns(getOperation().getContext());
+    populateConvertLinalgToTppPatterns(patterns);
+    linalg::populateFoldUnitExtentDimsPatterns(patterns);
     (void)applyPatternsAndFoldGreedily(getOperation(), std::move(patterns));
     return;
   }
--- test/Standalone/simple_match_and_replace.mlir
+++ test/Standalone/simple_match_and_replace.mlir
@@ -1,6 +1,6 @@
-// RUN: standalone-opt %s -split-input-file -linalg-bufferize -func-bufferize -map-linalg-to-tpp -convert-linalg-to-tpp | FileCheck %s
+// RUN: standalone-opt %s -split-input-file -linalg-bufferize -func-bufferize -map-linalg-to-tpp -convert-linalg-to-tpp -scf-parallel-loop-fusion -canonicalize | FileCheck %s
 
-// RUN: standalone-opt %s -split-input-file -map-linalg-to-tpp -linalg-bufferize -func-bufferize -convert-linalg-to-tpp | FileCheck %s
+// RUN: standalone-opt %s -split-input-file -map-linalg-to-tpp -linalg-bufferize -func-bufferize -convert-linalg-to-tpp -scf-parallel-loop-fusion -canonicalize | FileCheck %s
 
 #map0 = affine_map<(d0, d1) -> (d0, d1)>
 #map1 = affine_map<(d0, d1) -> (d0, d1)>
@@ -87,4 +87,25 @@ func.func @gemm(%arg0: tensor<1x256xf32>, %arg1: tensor<256x512xf32>) -> tensor<
     linalg.yield %3 : f32
   } -> tensor<1x512xf32> 
   return %1 : tensor<1x512xf32>
+}
+
+// -----
+
+#map0 = affine_map<(d0, d1, d2, d3) -> (d0, d1, d2, d3)>
+#map1 = affine_map<(d0, d1, d2, d3) -> (d0, d1, d2, d3)>
+
+func.func @identity(%arg1: tensor<32x33x34x35xf32>) -> tensor<32x33x34x35xf32> {
+  %0 = linalg.init_tensor [32, 33, 34, 35] : tensor<32x33x34x35xf32>
+  // CHECK: tpp.identity
+  %1 = linalg.generic {indexing_maps = [#map0, #map1], iterator_types = ["parallel", "parallel", "parallel", "parallel"]} ins(%arg1 : tensor<32x33x34x35xf32>) outs(%0 : tensor<32x33x34x35xf32>) {
+    ^bb0(%arg2: f32, %arg3: f32):
+      linalg.yield %arg2 : f32
+  } -> tensor<32x33x34x35xf32>
+  // CHECK: tpp.identity
+  %2 = linalg.generic {indexing_maps = [#map0, #map1], iterator_types = ["parallel", "parallel", "parallel", "parallel"]} ins(%arg1 : tensor<32x33x34x35xf32>) outs(%1 : tensor<32x33x34x35xf32>) {
+    ^bb0(%arg2: f32, %arg3: f32):
+      linalg.yield %arg2 : f32
+  } -> tensor<32x33x34x35xf32>
+
+  return %2 : tensor<32x33x34x35xf32>
 }
