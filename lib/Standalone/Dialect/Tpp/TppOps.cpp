561a83e8f9a6c6fd1a0dc45019bbb1dea9670aca
lorenzo chelini <l.chelini@icloud.com>
2022-06-14 16:05:22 +0200

    Add comment

--- lib/Standalone/Dialect/Tpp/TppOps.cpp
+++ lib/Standalone/Dialect/Tpp/TppOps.cpp
@@ -12,3 +12,15 @@
 
 #define GET_OP_CLASSES
 #include "Standalone/Dialect/Tpp/TppOps.cpp.inc"
+/*
+using namespace mlir;
+using namespace mlir::tpp;
+
+// TODO: For some reason hasVerifier = 1 does not add
+// the hook.
+LogicalResult IdentityOp::verify() {
+  1. The input can be a constant, 1d and 2 memref.
+  2. The output must no be a constant, but only
+  a 1d/2d memref.
+}
+*/
