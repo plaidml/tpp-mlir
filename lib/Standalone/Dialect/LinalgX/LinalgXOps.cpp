b24bb1f8be6cac1837d21d5df1f363ea5fc24a35
lorenzo chelini <l.chelini@icloud.com>
2022-07-25 16:54:35 +0200

    from block layout

--- lib/Standalone/Dialect/LinalgX/LinalgXOps.cpp
+++ lib/Standalone/Dialect/LinalgX/LinalgXOps.cpp
@@ -14,6 +14,10 @@
 using namespace mlir;
 using namespace mlir::linalgx;
 
+//===----------------------------------------------------------------------===//
+// ToBlockLayout
+//===----------------------------------------------------------------------===//
+
 ArrayAttr ToBlockLayout::getIndexingMaps() {
   MLIRContext *context = getContext();
   auto maybeInputMap = getInputMap();
@@ -37,6 +41,39 @@ std::string ToBlockLayout::getLibraryCallName() { return "to_block_layout"; }
 
 void ToBlockLayout::regionBuilder(ImplicitLocOpBuilder &b, Block &block,
                                   llvm::ArrayRef<NamedAttribute> attrs = {}) {
+  assert(block.getNumArguments() == 2 && "CopyOp regionBuilder expects 2 args");
+  b.create<linalg::YieldOp>(block.getArgument(0));
+}
+
+//===----------------------------------------------------------------------===//
+// FromBlockLayout
+//===----------------------------------------------------------------------===//
+
+ArrayAttr FromBlockLayout::getIndexingMaps() {
+  MLIRContext *context = getContext();
+  auto maybeInputMap = getInputMap();
+  auto maybeOutputMap = getOutputMap();
+  int64_t inputRank = getRank(getInputOperand(0));
+  int64_t outputRank = getRank(getOutputOperand(0));
+  return Builder(getContext())
+      .getAffineMapArrayAttr(
+          {linalg::extractOrIdentityMap(maybeInputMap, inputRank, context),
+           linalg::extractOrIdentityMap(maybeOutputMap, outputRank, context)});
+}
+
+ArrayAttr FromBlockLayout::iterator_types() {
+  int64_t numLoops = getTiedIndexingMap(getInputOperand(0)).getNumDims();
+  return Builder(getContext())
+      .getStrArrayAttr(
+          SmallVector<StringRef, 8>(numLoops, getParallelIteratorTypeName()));
+}
+
+std::string FromBlockLayout::getLibraryCallName() {
+  return "from_block_layout";
+}
+
+void FromBlockLayout::regionBuilder(ImplicitLocOpBuilder &b, Block &block,
+                                    llvm::ArrayRef<NamedAttribute> attrs = {}) {
   assert(block.getNumArguments() == 2 && "CopyOp regionBuilder expects 2 args");
   b.create<linalg::YieldOp>(block.getArgument(0));
 }
--- test/Standalone/linalgx_ops.mlir
+++ test/Standalone/linalgx_ops.mlir
@@ -3,7 +3,7 @@
 #mapI = affine_map<(n1, c1, n2, c2) -> (n1 * 2 + n2, c1 * 2 + c2)>
 #mapO = affine_map<(n1, c1, n2, c2) -> (n1, c1, n2, c2)>
 
-func.func @myfunc(%arg0: memref<3x3xf32>, %arg1: memref<1x1x3x3xf32>) -> memref<1x1x3x3xf32> {  
+func.func @toblockmemref(%arg0: memref<3x3xf32>, %arg1: memref<1x1x3x3xf32>) -> memref<1x1x3x3xf32> {  
   // CHECK: linalgx.to_block
   linalgx.to_block ins(%arg0: memref<3x3xf32>, #mapI) 
                   outs(%arg1: memref<1x1x3x3xf32>, #mapO) {
@@ -13,7 +13,17 @@ func.func @myfunc(%arg0: memref<3x3xf32>, %arg1: memref<1x1x3x3xf32>) -> memref<
   return %arg1: memref<1x1x3x3xf32>
 }
 
-func.func @myfunctensor(%arg0: tensor<3x3xf32>, %arg1: tensor<1x1x3x3xf32>) -> tensor<1x1x3x3xf32> {
+func.func @fromblockmemref(%arg0: memref<3x3xf32>, %arg1: memref<1x1x3x3xf32>) -> memref<3x3xf32> {
+  // CHECK: linalgx.from_block
+  linalgx.from_block ins(%arg1: memref<1x1x3x3xf32>, #mapO) 
+                  outs(%arg0: memref<3x3xf32>, #mapI) {
+    ^bb0(%arg2: f32, %arg3: f32):
+      linalg.yield %arg2: f32
+  }
+  return %arg0: memref<3x3xf32>
+}
+
+func.func @toblocktensor(%arg0: tensor<3x3xf32>, %arg1: tensor<1x1x3x3xf32>) -> tensor<1x1x3x3xf32> {
   // CHECK: linalgx.to_block
   %0 = linalgx.to_block ins(%arg0: tensor<3x3xf32>, #mapI) 
                   outs(%arg1: tensor<1x1x3x3xf32>, #mapO) {
@@ -21,4 +31,14 @@ func.func @myfunctensor(%arg0: tensor<3x3xf32>, %arg1: tensor<1x1x3x3xf32>) -> t
       linalg.yield %arg2: f32
   } -> tensor<1x1x3x3xf32>
   return %0: tensor<1x1x3x3xf32>
+}
+
+func.func @fromblocktensor(%arg0: tensor<3x3xf32>, %arg1: tensor<1x1x3x3xf32>) -> tensor<3x3xf32> {
+  // CHECK: linalgx.from_block
+  %0 = linalgx.from_block ins(%arg1: tensor<1x1x3x3xf32>, #mapO) 
+                  outs(%arg0: tensor<3x3xf32>, #mapI) {
+    ^bb0(%arg2: f32, %arg3: f32):
+      linalg.yield %arg2: f32
+  } -> tensor<3x3xf32>
+  return %0: tensor<3x3xf32>
 }
