f46fb3f3af96592594180586fecff997ccc3b14f
lorenzo chelini <l.chelini@icloud.com>
2022-07-26 18:30:08 +0200

    bufferization

--- lib/Standalone/Dialect/LinalgX/BufferizableOpInterfaceImpl.cpp
+++ lib/Standalone/Dialect/LinalgX/BufferizableOpInterfaceImpl.cpp
@@ -15,17 +15,104 @@ using namespace mlir::bufferization;
 using namespace mlir::linalgx;
 
 namespace mlir {
-namespace linalx {
+namespace linalgx {
 namespace {
 
-/* write something here */
+struct ToBlockLayoutInterface
+    : public BufferizableOpInterface::ExternalModel<ToBlockLayoutInterface,
+                                                    linalgx::ToBlockLayout> {
+  bool bufferizesToMemoryRead(Operation *op, OpOperand &opOperand,
+                              const AnalysisState &state) const {
+    return true;
+  }
+
+  bool bufferizesToMemoryWrite(Operation *op, OpOperand &opOperand,
+                               const AnalysisState &state) const {
+    return true;
+  }
+
+  SmallVector<OpResult> getAliasingOpResult(Operation *op, OpOperand &opOperand,
+                                            const AnalysisState &state) const {
+    return {op->getOpResult(0)};
+  }
+
+  BufferRelation bufferRelation(Operation *op, OpResult opResult,
+                                const AnalysisState &state) const {
+    return BufferRelation::None;
+  }
+
+  LogicalResult bufferize(Operation *op, RewriterBase &rewriter,
+                          const BufferizationOptions &options) const {
+    // OpBuilder::InsertionGuard g(rewriter);
+    linalgx::ToBlockLayout toBlockLayout = cast<linalgx::ToBlockLayout>(op);
+
+    FailureOr<Value> destBuffer =
+        getBuffer(rewriter, toBlockLayout.outputs()[0], options);
+    if (failed(destBuffer))
+      return failure();
+
+    FailureOr<Value> srcBuffer =
+        getBuffer(rewriter, toBlockLayout.inputs()[0], options);
+    if (failed(srcBuffer))
+      return failure();
+
+    replaceOpWithBufferizedValues(rewriter, op, {srcBuffer, destBuffer});
+    return success();
+  }
+};
+
+struct FromBlockLayoutInterface
+    : public BufferizableOpInterface::ExternalModel<FromBlockLayoutInterface,
+                                                    linalgx::FromBlockLayout> {
+  bool bufferizesToMemoryRead(Operation *op, OpOperand &opOperand,
+                              const AnalysisState &state) const {
+    return true;
+  }
+
+  bool bufferizesToMemoryWrite(Operation *op, OpOperand &opOperand,
+                               const AnalysisState &state) const {
+    return true;
+  }
+
+  SmallVector<OpResult> getAliasingOpResult(Operation *op, OpOperand &opOperand,
+                                            const AnalysisState &state) const {
+    return {op->getOpResult(0)};
+  }
+
+  BufferRelation bufferRelation(Operation *op, OpResult opResult,
+                                const AnalysisState &state) const {
+    return BufferRelation::None;
+  }
+
+  LogicalResult bufferize(Operation *op, RewriterBase &rewriter,
+                          const BufferizationOptions &options) const {
+    linalgx::FromBlockLayout fromBlockLayout =
+        cast<linalgx::FromBlockLayout>(op);
+
+    FailureOr<Value> destBuffer =
+        getBuffer(rewriter, fromBlockLayout.outputs()[0], options);
+    if (failed(destBuffer))
+      return failure();
+
+    FailureOr<Value> srcBuffer =
+        getBuffer(rewriter, fromBlockLayout.inputs()[0], options);
+    if (failed(srcBuffer))
+      return failure();
+
+    replaceOpWithBufferizedValues(rewriter, op, {srcBuffer, destBuffer});
+    return success();
+  }
+};
 
 } // namespace
-} // namespace linalx
+} // namespace linalgx
 } // namespace mlir
 
 void mlir::linalgx::registerBufferizableOpInterfaceExternalModels(
     DialectRegistry &registry) {
-  registry.addExtension(
-      +[](MLIRContext *ctx, linalgx::LinalgXDialect *dialect) {});
+  registry.addExtension(+[](MLIRContext *ctx,
+                            linalgx::LinalgXDialect *dialect) {
+    ToBlockLayout::attachInterface<linalgx::ToBlockLayoutInterface>(*ctx);
+    FromBlockLayout::attachInterface<linalgx::FromBlockLayoutInterface>(*ctx);
+  });
 }
---
+++ test/Standalone/bufferize-linalgx.mlir
@@ -0,0 +1,12 @@
+
+#map0 = affine_map<(d0, d1, d2, d3) -> (d0 * 32 + d2, d1 * 32 + d3)>
+#map1 = affine_map<(d0, d1, d2, d3) -> (d0, d1, d2, d3)>
+
+func.func @myfunc(%arg0: tensor<128x256xf32>) -> tensor<4x8x32x32xf32> {
+  %0 = linalg.init_tensor [4, 8, 32, 32] : tensor<4x8x32x32xf32>
+  %1 = linalgx.to_block ins(%arg0 : tensor<128x256xf32>, #map0) outs(%0 : tensor<4x8x32x32xf32>, #map1) {
+    ^bb0(%arg4: f32, %arg5: f32):
+      linalg.yield %arg4 : f32
+  } -> tensor<4x8x32x32xf32> {operand_segment_sizes = dense<1> : vector<2xi32>}
+  return %1 : tensor<4x8x32x32xf32>
+}
