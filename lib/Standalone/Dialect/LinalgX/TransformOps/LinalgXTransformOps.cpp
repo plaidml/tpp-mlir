f8dce7db10970bbc8da10690337ebdc0b668ae8c
lorenzo chelini <l.chelini@icloud.com>
2022-09-12 16:07:54 +0200

    nit

--- lib/Standalone/Dialect/LinalgX/TransformOps/LinalgXTransformOps.cpp
+++ lib/Standalone/Dialect/LinalgX/TransformOps/LinalgXTransformOps.cpp
@@ -1,5 +1,4 @@
-//===- LinalgXTransformOps.cpp - Implementation of LinalgX transform ops
-//---===//
+//===- LinalgXTransformOps.cpp - Implementation of LinalgX transform ops--====//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
