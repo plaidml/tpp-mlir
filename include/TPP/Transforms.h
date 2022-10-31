354477bef5ec334ac50c86a11f263c6d0ec076b1
Lorenzo Chelini <l.chelini@icloud.com>
2022-10-31 18:04:48 +0100

    Address comment

--- include/TPP/Transforms.h
+++ include/TPP/Transforms.h
@@ -29,7 +29,6 @@ namespace linalgx {
 FailureOr<SmallVector<Value>> mapToBRGEMMOp(RewriterBase &rewriter,
                                             linalg::LinalgOp linalgOp);
 
-// TODO: this is now more generic.
 // Map a convolution to a matmul operation. We support the following formats:
 // 1. [N][P][Q][K] += [N][H][W][C] * [R][S][C][K]
 // 2. [N][K’][P][Q][k] += [N][C’][H][W][c] * [K’][C’][R][S][c][k] (blocked)
