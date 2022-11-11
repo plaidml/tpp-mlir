081ba6a5f0db9265d5e89ba163aead9d7e7e9d95
Kavitha <kavitha.madhu@intel.com>
2022-11-11 16:37:12 +0530

    Check to SCF Loops conversion pass (#145)

---
+++ lib/TPP/ConvertCheckToLoops.cpp
@@ -0,0 +1,130 @@
+//===- ConvertCheckToLoops.cpp ----------------------------------*- C++-*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "TPP/Dialect/Check/CheckOps.h"
+#include "TPP/Passes.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlowOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Linalg/Utils/Utils.h"
+#include "mlir/Dialect/Math/IR/Math.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+using namespace mlir;
+using namespace mlir::check;
+using namespace mlir::cf;
+using namespace mlir::arith;
+using namespace mlir::math;
+
+#define GEN_PASS_CLASSES
+#include "TPP/Passes.h.inc"
+
+namespace {
+
+// Convert 
+// check.expect_almost_equals(%t1:tensor<2x2xf32>, %t2:tensor<2x2xf32>,
+// %threshold:f32) 
+// to the pattern: 
+// scf.some_loop(%i, %j)
+// %0 = load from %t1 
+// %1 = load from %t2 
+// %diff = math.subf %0, %1 
+// %abs = arith.absf %diff 
+// %compare = arith.cmpf le, %abs, %threshold 
+// cf.assert %compare, "Result mismatch"
+struct ConvertAlmostEqualsOp
+    : public OpRewritePattern<check::ExpectAlmostEqOp> {
+  using OpRewritePattern<check::ExpectAlmostEqOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(check::ExpectAlmostEqOp almostEqOp,
+                                PatternRewriter &rewriter) const override {
+    Location loc = almostEqOp.getLoc();
+    SmallVector<Value> ubs;
+    if (!almostEqOp.getLhs().getType().isa<MemRefType>()) {
+      return failure();
+    }
+    size_t rank = almostEqOp.getLhs().getType().cast<MemRefType>().getRank();
+    for (size_t idx = 0; idx < rank; idx++) {
+      auto dim = linalg::createOrFoldDimOp(rewriter, loc,
+                                           almostEqOp.getOperand(0), idx);
+      ubs.push_back(dim);
+    }
+    Value zero = rewriter.create<arith::ConstantIndexOp>(loc, 0);
+    SmallVector<Value> lbs(rank, zero);
+    Value one = rewriter.create<arith::ConstantIndexOp>(loc, 1);
+    SmallVector<Value> steps(rank, one);
+    (void)scf::buildLoopNest(
+        rewriter, loc, lbs, ubs, steps,
+        [&](OpBuilder &b, Location loc, ValueRange localIvs) {
+          Value scalarLhs =
+              b.create<memref::LoadOp>(loc, almostEqOp.getLhs(), localIvs);
+          Value scalarRhs =
+              b.create<memref::LoadOp>(loc, almostEqOp.getRhs(), localIvs);
+          Value compare;
+          if (scalarLhs.getType().isa<mlir::FloatType>()) {
+            Value diff = b.create<arith::SubFOp>(loc, scalarLhs, scalarRhs);
+            Value abs = b.create<math::AbsFOp>(loc, diff);
+            compare = b.create<arith::CmpFOp>(loc, arith::CmpFPredicate::OLE,
+                                              abs, almostEqOp.getThreshold());
+          } else {
+            Value diff = b.create<arith::SubIOp>(loc, scalarLhs, scalarRhs);
+            Value abs = b.create<math::AbsIOp>(loc, diff);
+            compare = b.create<arith::CmpIOp>(loc, arith::CmpIPredicate::sle,
+                                              abs, almostEqOp.getThreshold());
+          }
+          b.create<cf::AssertOp>(loc, compare,
+                                 b.getStringAttr("Result mismatch"));
+        });
+    rewriter.eraseOp(almostEqOp);
+    return success();
+  }
+};
+
+// Converts check.expect_true %true to
+// cf.assert %true, "Result mismatch"
+struct ConvertExpectTrueOp : public OpRewritePattern<ExpectTrueOp> {
+  using OpRewritePattern<ExpectTrueOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ExpectTrueOp expectTrueOp,
+                                PatternRewriter &rewriter) const override {
+    Location loc = expectTrueOp.getLoc();
+    IntegerType i1 = IntegerType::get(rewriter.getContext(), 1);
+    Value one = rewriter.create<arith::ConstantOp>(
+        loc, i1, rewriter.getIntegerAttr(i1, 1));
+    Value compare = rewriter.create<arith::CmpIOp>(
+        loc, arith::CmpIPredicate::eq, expectTrueOp.getOperand(), one);
+    rewriter.create<cf::AssertOp>(loc, compare,
+                                  rewriter.getStringAttr("Result mismatch"));
+    rewriter.eraseOp(expectTrueOp);
+    return success();
+  }
+};
+
+void populateCheckToLoopsPatterns(RewritePatternSet &patterns) {
+  patterns.add<ConvertAlmostEqualsOp, ConvertExpectTrueOp>(
+      patterns.getContext());
+}
+
+struct ConvertCheckToLoops
+    : public ConvertCheckToLoopsBase<ConvertCheckToLoops> {
+  void runOnOperation() override {
+    RewritePatternSet patterns(&getContext());
+    populateCheckToLoopsPatterns(patterns);
+    (void)applyPatternsAndFoldGreedily(getOperation(), std::move(patterns));
+    return;
+  }
+};
+
+} // namespace
+
+std::unique_ptr<OperationPass<ModuleOp>>
+mlir::tpp::createConvertCheckToLoopsPass() {
+  return std::make_unique<ConvertCheckToLoops>();
+}
