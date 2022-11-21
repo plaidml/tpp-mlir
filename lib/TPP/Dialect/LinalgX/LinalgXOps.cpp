e0f825ec07c3ea13e0e98a597a3dce05c9da1fce
Lorenzo Chelini <l.chelini@icloud.com>
2022-11-21 15:57:39 +0100

    fix bug in unpack lowering

--- lib/TPP/Dialect/LinalgX/LinalgXOps.cpp
+++ lib/TPP/Dialect/LinalgX/LinalgXOps.cpp
@@ -802,9 +802,7 @@ LogicalResult UnPackOp::generateScalarImplementation(OpBuilder &builder,
   // interchange the tiled loops induction variables based on `outer_dims_pos`.
   SmallVector<int64_t> outerDims = extractFromI64ArrayAttr(getOuterDimsPerm());
   if (!outerDims.empty()) {
-    interchangeVector =
-        computeInterchangeFromDimPos(outerDims, getOutputRank());
-    inputIvs = interchange<Value>(inputIvs, interchangeVector, /*offset=*/0);
+    inputIvs = interchange<Value>(inputIvs, outerDims, /*offset=*/0);
   }
 
   llvm::append_range(inputIvs, interchangedInputIvsPointLoops);
