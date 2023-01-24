9907c496de028fa69154d2aa14a3054dcc76f771
Lorenzo Chelini <l.chelini@icloud.com>
2023-01-24 11:08:49 +0100

    Fix position of header guards (NFC)

--- include/TPP/TransformUtils.h
+++ include/TPP/TransformUtils.h
@@ -1,6 +1,3 @@
-#ifndef TPP_TRANSFORMUTILS_H
-#define TPP_TRANSFORMUTILS_H
-
 //===- TransformUtils.cpp ----------------------------------------*- C++-*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
@@ -8,6 +5,9 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+
+#ifndef TPP_TRANSFORMUTILS_H
+#define TPP_TRANSFORMUTILS_H
 
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/Interfaces/ViewLikeInterface.h"
