b2c271645deebe22b2d09466bde51abc53d8c14c
Adam Siemieniuk <adam.siemieniuk@intel.com>
2023-02-02 14:07:43 +0100

    Rename DefaultTppPasses -> UtilityPasses
    Create a placeholder for a collection of various utility passes.
    They do not introduce any new transformation logic but simply
    group and execute other existing passes.

--- lib/TPP/UtilityPasses.cpp
+++ lib/TPP/UtilityPasses.cpp
@@ -1,4 +1,4 @@
-//===- DefaultTppPasses.cpp --------------------------------------*- C++-*-===//
+//===- UtilityPasses.cpp -----------------------------------------*- C++-*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
