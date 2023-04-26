a10d1c3c570bb379ecd8754df706cc8def2e0919
Alexander Heinecke <heinecke.alex@gmail.com>
2023-04-25 18:28:51 -0700

    temp. workaround to enable different VNNI packing on x86_64 & aarch64

--- lib/TPP/VNNIUtils.cpp
+++ lib/TPP/VNNIUtils.cpp
@@ -16,7 +16,15 @@ Optional<int64_t> getVNNIBlockingFactor(Type type) {
   if (!isa<ShapedType>(type) ||
       !type.cast<ShapedType>().getElementType().isBF16())
     return std::nullopt;
+  // @ TODO we should call LIBXSMM here to query the 
+  // VNNI packing factor we need to match to
+#if defined(__x86_64__)
   return 2;
+#elif defined(__aarch64__)
+  return 4;
+#else
+#error Unsupported architecture
+#endif
 }
 
 bool isBF16Type(Type type) {
