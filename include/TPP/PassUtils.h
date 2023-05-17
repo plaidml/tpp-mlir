04d62453152b39769d748798fce62fe91909beef
Adam Siemieniuk <adam.siemieniuk@intel.com>
2023-05-17 14:48:04 +0200

    Fix header

--- include/TPP/PassUtils.h
+++ include/TPP/PassUtils.h
@@ -1,5 +1,12 @@
-//===- Pass Utils - Helper for pass creation  -----------------------------===//
-// Utilities to help build MLIR passes
+//===- PassUtils.h - Helpers for pass creation ----------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Utilities to help build MLIR passes.
 //
 //===----------------------------------------------------------------------===//
 
