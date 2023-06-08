plaidml/tpp-mlir

commit 95e434a2b185e43066aa3b30eb03d93d908184f5

Remove unnecessary helper (NFC)

lib/TPP/ConvertLinalgToTpp.cpp
@@ -78,7 +78,7 @@ struct ConvertGenericOpToTpp : public OpRewritePattern<linalg::GenericOp> {
 
   LogicalResult matchAndRewrite(linalg::GenericOp linalgOp,
                                 PatternRewriter &rewriter) const override {
-    if (!tpp::utils::hasStaticShape(linalgOp))
+    if (linalgOp.hasDynamicShape())
       return rewriter.notifyMatchFailure(
           linalgOp, "Expect static shape when mapping to tpp");
     return rewriteToTppOp(linalgOp, rewriter);
@@ -92,7 +92,7 @@ struct ConvertBrgemmToTpp
 
   LogicalResult matchAndRewrite(linalg::BatchReduceMatmulOp brMatmulOp,
                                 PatternRewriter &rewriter) const override {
-    if (!tpp::utils::hasStaticShape(brMatmulOp))
+    if (brMatmulOp.hasDynamicShape())
       return rewriter.notifyMatchFailure(
           brMatmulOp, "Expect static shape when mapping to tpp");
     SmallVector<Value> inputs = brMatmulOp.getDpsInputOperands();
@@ -109,7 +109,7 @@ struct ConvertMatmulToTpp : public OpRewritePattern<linalg::MatmulOp> {
 
   LogicalResult matchAndRewrite(linalg::MatmulOp matmulOp,
                                 PatternRewriter &rewriter) const override {
-    if (!tpp::utils::hasStaticShape(matmulOp))
+    if (matmulOp.hasDynamicShape())
       return rewriter.notifyMatchFailure(
           matmulOp, "Expect static shape when mapping to tpp");
     SmallVector<Value> inputs = matmulOp.getDpsInputOperands();
@@ -126,7 +126,7 @@ struct ConvertFillToTpp : public OpRewritePattern<linalg::FillOp> {
 
   LogicalResult matchAndRewrite(linalg::FillOp fillOp,
                                 PatternRewriter &rewriter) const override {
-    if (!tpp::utils::hasStaticShape(fillOp))
+    if (fillOp.hasDynamicShape())
       return rewriter.notifyMatchFailure(
           fillOp, "Expect static shape when mapping to tpp");
 
@@ -159,7 +159,7 @@ struct ConvertLinalgToTpp : public ConvertLinalgToTppBase<ConvertLinalgToTpp> {
 
 void mlir::tpp::populateConvertLinalgToTppPatterns(
     RewritePatternSet &patterns) {
-  // clang-format off
+  // cla/hasDynng-format off
   patterns.add<ConvertGenericOpToTpp,
                ConvertBrgemmToTpp,
                ConvertMatmulToTpp,

include/TPP/Dialect/Tpp/TppUtils.h
@@ -26,10 +26,6 @@ class YieldOp;
 namespace tpp {
 namespace utils {
 
-// Returns true if all the operands of the linalg operation have static
-// dimensions.
-bool hasStaticShape(linalg::LinalgOp linalgOp);
-
 // Returns true if the linalg operation has been marked by the tpp detection
 // pass and the operation can be mapped to a tpp operation.
 bool hasTppMark(linalg::LinalgOp linalgOp);

lib/TPP/Dialect/Tpp/TppUtils.cpp
@@ -25,10 +25,6 @@ namespace mlir {
 namespace tpp {
 namespace utils {
 
-bool hasStaticShape(linalg::LinalgOp linalgOp) {
-  return !linalgOp.hasDynamicShape();
-}
-
 bool hasTppMark(linalg::LinalgOp linalgOp) {
   // Here we are abusing a bit the linalg library name machinery.
   // Main asserts if we query the name at tensor level. Inspect

commit 5321dd56d848a3bd8a8117df9696b1dcf9d97cc8

Fix typo in `ConvertLinalgToTpp.cpp` (NFC) (#574)
Introduced in
https://github.com/plaidml/tpp-mlir/commit/95e434a2b185e43066aa3b30eb03d93d908184f5

lib/TPP/ConvertLinalgToTpp.cpp
@@ -159,7 +159,7 @@ struct ConvertLinalgToTpp : public ConvertLinalgToTppBase<ConvertLinalgToTpp> {
 
 void mlir::tpp::populateConvertLinalgToTppPatterns(
     RewritePatternSet &patterns) {
-  // cla/hasDynng-format off
+  // clang-format off
   patterns.add<ConvertGenericOpToTpp,
                ConvertBrgemmToTpp,
                ConvertMatmulToTpp,