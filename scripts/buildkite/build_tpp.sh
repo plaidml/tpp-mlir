7a5bae7ae76d91491af7fb924124e89d630d557d
Hans Pabst <hans.pabst@intel.com>
2023-06-26 12:50:07 +0200

    Prepend build directory to list of search-directories for shared libraries.

--- scripts/buildkite/build_tpp.sh
+++ scripts/buildkite/build_tpp.sh
@@ -79,6 +79,7 @@ if [ ! -f "${CUDA_DRIVER}" ]; then
     echo "Creating links to CUDA stubs"
     ln -s ${CUDATOOLKIT_HOME}/lib64/stubs/libcuda.so ${BLD_DIR}/lib/libcuda.so.1
     ln -s ${BLD_DIR}/lib/libcuda.so.1 ${BLD_DIR}/lib/libcuda.so
+    export LD_LIBRARY_PATH=${BLD_DIR}/lib:${LD_LIBRARY_PATH}
   fi
 fi
 
