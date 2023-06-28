0dd3f89c4e779de7829a4b8ac979bb35067e0f7f
Adam Siemieniuk <adam.siemieniuk@intel.com>
2023-06-28 12:20:35 +0200

    Add file headers

--- benchmarks/ref/include/Tensor.h
+++ benchmarks/ref/include/Tensor.h
@@ -1,3 +1,11 @@
+//===- Tensor.h - ---------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #pragma once
 
 #include "llvm/ADT/STLExtras.h"
--- benchmarks/ref/matmul/matmul.cpp
+++ benchmarks/ref/matmul/matmul.cpp
@@ -1,3 +1,11 @@
+//===- matmul.cpp ------------------------------------------------*- C++-*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "Config.h"
 #include "Bench.h"
 #include "Tensor.h"
