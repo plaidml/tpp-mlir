5c9df38d8f6472ede007ac89bc32f5db97c2e470
Adam Siemieniuk <adam.siemieniuk@intel.com>
2023-06-29 12:09:43 +0200

    Cuda tensor helper struct

---
+++ benchmarks/ref/GPU/cuda/CudaTensor.h
@@ -0,0 +1,63 @@
+//===- CudaTensor.h - -----------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "Tensor.h"
+#include "llvm/ADT/STLExtras.h"
+
+#include <cassert>
+#include <cstring>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+#include <cuda_runtime.h>
+
+namespace {
+using ListArg = std::vector<unsigned>;
+using InitArg = std::initializer_list<unsigned>;
+} // namespace
+
+/// Tensor: represents a tensor in memory, basically a pointer to data plus
+/// rank and dimension.
+template <typename T> struct CudaTensor {
+  CudaTensor() = delete;
+  CudaTensor(Tensor<T> &&tensor) : tensor(std::move(tensor)) {}
+
+  virtual ~CudaTensor() {
+    if (gpuData)
+      cudaFree(gpuData);
+  }
+
+  bool initGpu() {
+    if (gpuData)
+      cudaFree(gpuData);
+
+    auto dataSize = tensor.getDataSize();
+    auto allocErr = cudaMalloc(&gpuData, dataSize);
+    if (allocErr != cudaSuccess) {
+      std::cerr << "GPU allocation error\n";
+      return false;
+    }
+
+    auto data = tensor.getData();
+    auto cpyErr = cudaMemcpy(gpuData, data, dataSize, cudaMemcpyHostToDevice);
+    if (cpyErr != cudaSuccess) {
+      std::cerr << "GPU memcpy error\n";
+      return false;
+    }
+
+    return true;
+  }
+
+  T *gpuData = nullptr;
+  Tensor<T> tensor;
+};
