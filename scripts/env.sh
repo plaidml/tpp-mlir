b7269dc851929fe679464f878a14065b64339fe3
Hans Pabst <hans.pabst@intel.com>
2023-07-04 09:58:36 +0200

    Added script to source the runtime environment namely the LLVM-build used to build TPP-mlir.

---
+++ scripts/env.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+#
+# Setup runtime environment based on build_tools/llvm_version.txt
+
+if [ ! "${TPP_LLVM}" ] && [ -d /nfs_home/buildkite-slurm/builds/tpp ]; then
+  source /nfs_home/buildkite-slurm/builds/tpp/enable-tpp
+fi
+
+if [ "${TPP_LLVM}" ]; then
+  # basic utilities functions (git_root, llvm_version)
+  source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/ci/common.sh"
+
+  # LLVM version used to build TPP-mlir
+  TPP_LLVM_VERSION=$(llvm_version)
+
+  if [ "${TPP_LLVM_VERSION}" ]; then
+    # setup environment
+    export TPP_LLVM_VERSION
+    export TPP_LLVM_DIR=${TPP_LLVM}/${TPP_LLVM_VERSION}
+    # avoid overriding PATH/LD_LIBRARY_PATH of initial environment (append)
+    export PATH=${PATH}:${TPP_LLVM_DIR}/bin
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TPP_LLVM_DIR}/lib
+
+    # setup additional/legacy envronment variables
+    export CUSTOM_LLVM_ROOT=${TPP_LLVM_DIR}
+    export LLVM_VERSION=${TPP_LLVM_VERSION}
+  else
+    echo "ERROR: Cannot determine LLVM-version!"
+  fi
+else
+  echo "ERROR: Please source the TPP-environment first!"
+fi
