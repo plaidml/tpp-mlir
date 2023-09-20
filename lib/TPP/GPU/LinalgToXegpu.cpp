d8ce1f629a5d3b0709f02c936d8e4b17953fb90f
Adam Siemieniuk <adam.siemieniuk@intel.com>
2023-09-20 20:34:27 +0200

    Linalg to XeGPU lowering

--- lib/TPP/GPU/LinalgToXegpu.cpp
+++ lib/TPP/GPU/LinalgToXegpu.cpp
@@ -44,7 +44,7 @@ namespace {
 static std::optional<scf::ParallelOp>
 createGpuBlocksWrapper(Operation *op, ArrayRef<int64_t> blockDims,
                        PatternRewriter &rewriter) {
-  assert(blockDims.size() <= 3 && "Too many GPU blocks dimensions");
+  assert(blockDims.size() <= 3 && "Too many GPU block dimensions");
 
   auto loc = op->getLoc();
 
@@ -69,51 +69,17 @@ createGpuBlocksWrapper(Operation *op, ArrayRef<int64_t> blockDims,
   return rewriter.create<scf::ParallelOp>(loc, lbs, gpuBlocks, steps);
 }
 
-// Return true if the operation can be represented with WMMA compute.
-static bool supportsMMACompute(linalg::LinalgOp linalgOp) {
-  if (!(isa_and_nonnull<linalg::MatmulOp>(linalgOp) ||
-        isa_and_nonnull<linalg::BatchReduceMatmulOp>(linalgOp))) {
-    return false;
-  }
-
-  // Only static shapes are supported.
-  if (linalgOp.hasDynamicShape())
-    return false;
-
-  auto aType =
-      linalgOp.getDpsInputOperands()[0]->get().getType().cast<ShapedType>();
-  auto bType =
-      linalgOp.getDpsInputOperands()[1]->get().getType().cast<ShapedType>();
-  auto cType =
-      linalgOp.getDpsInitOperands()[0]->get().getType().cast<ShapedType>();
-
-  ArrayRef<int64_t> shapeA = aType.getShape();
-  ArrayRef<int64_t> shapeC = cType.getShape();
-  int64_t m = shapeC[0];
-  int64_t n = shapeC[1];
-  // Buffer A might be 2D (gemm) or 3D (brgemm) but the last dimension will
-  // always be reduction.
-  int64_t k = shapeA.back();
-
-  // For now, only M-N-K F16[16] x F16[16] x F16[16] WMMA variant is supported.
-  // TODO: add more WMMA combinations.
-  return aType.getElementType().isF16() && bType.getElementType().isF16() &&
-         cType.getElementType().isF16() && m == 16 && n == 16 && k == 16;
-}
-
-// Create WMMA instructions out of matmul-like operation.
-static LogicalResult gemmToGpuMMA(linalg::LinalgOp linalgOp,
-                                  PatternRewriter &rewriter) {
+// Create XeGPU instructions out of matmul-like operation.
+static LogicalResult gemmToXegpu(linalg::LinalgOp linalgOp,
+                                 PatternRewriter &rewriter) {
   assert((isa_and_nonnull<linalg::MatmulOp>(linalgOp) ||
           isa_and_nonnull<linalg::BatchReduceMatmulOp>(linalgOp)) &&
-         "Requires a matmul like op for MMA lowering");
+         "Requires a matmul like op for XeGPU lowering");
 
   Location loc = linalgOp.getLoc();
 
   // If there is no parallel loop, create a unit blocks wrapper around the
-  // current op.
-  // This ensures that WMMA operations are created at the thread level (inner
-  // nested parallel loops).
+  // current op. This allows for kernel outlining later on.
   auto blocksLoop = createGpuBlocksWrapper(linalgOp, {1, 1}, rewriter);
   if (blocksLoop)
     rewriter.setInsertionPoint(blocksLoop->getBody()->getTerminator());
@@ -126,54 +92,35 @@ static LogicalResult gemmToGpuMMA(linalg::LinalgOp linalgOp,
   auto typeB = matB.getType().cast<ShapedType>();
   auto typeC = matC.getType().cast<ShapedType>();
 
-  gpu::MMAMatrixType mmaTypeA = gpu::MMAMatrixType::get(
-      typeA.getShape().take_back(2), typeA.getElementType(), "AOp");
-  gpu::MMAMatrixType mmaTypeB = gpu::MMAMatrixType::get(
-      typeB.getShape().take_back(2), typeB.getElementType(), "BOp");
-  gpu::MMAMatrixType mmaTypeC =
-      gpu::MMAMatrixType::get(typeC.getShape(), typeC.getElementType(), "COp");
-
-  auto stridesA = utils::getStaticStrides(matA);
-  auto stridesB = utils::getStaticStrides(matB);
-  auto stridesC = utils::getStaticStrides(matC);
-
-  if (failed(stridesA) || failed(stridesB) || failed(stridesC)) {
-    return rewriter.notifyMatchFailure(
-        linalgOp, "Expect static strides for MMA lowering");
-  }
-  if (stridesA->back() != 1 || stridesB->back() != 1 || stridesC->back() != 1) {
-    return rewriter.notifyMatchFailure(
-        linalgOp,
-        "Expect unit stride in the innermost dimension for MMA operations");
-  }
+  // Skip batch dimension stride in case of brgemm.
+  auto tileTypeA = xegpu::TileType::get(typeA.getShape().take_back(2),
+                                        typeA.getElementType());
+  auto tileTypeB = xegpu::TileType::get(typeB.getShape().take_back(2),
+                                        typeB.getElementType());
+  auto tileTypeC =
+      xegpu::TileType::get(typeC.getShape(), typeC.getElementType());
 
   bool isBrgemm = isa<linalg::BatchReduceMatmulOp>(linalgOp);
 
-  // Skip batch dimension stride in case of brgemm.
-  auto lda = rewriter.getIndexAttr(stridesA->begin()[isBrgemm ? 1 : 0]);
-  auto ldb = rewriter.getIndexAttr(stridesB->begin()[isBrgemm ? 1 : 0]);
-  auto ldc = rewriter.getIndexAttr(stridesC->front());
-
   Value zero = rewriter.create<arith::ConstantIndexOp>(loc, 0);
   Value one = rewriter.create<arith::ConstantIndexOp>(loc, 1);
-  // WMMA requires warp/subgroup size of 32 threads/work items.
-  Value subgroupSize = rewriter.create<arith::ConstantIndexOp>(loc, 32);
-
-  // Create parallel loop to indicate that the whole subgroup is performing MMA
-  // operations together. It also ensures that the kernel is outlined with
-  // the correct number of threads.
-  auto parallelLoop = rewriter.create<scf::ParallelOp>(
-      loc, ValueRange{zero}, ValueRange{subgroupSize}, ValueRange{one});
 
   OpBuilder::InsertionGuard guard(rewriter);
-  rewriter.setInsertionPoint(parallelLoop.getBody()->getTerminator());
 
   // Fetch the inital value of the output element.
-  Value tileC = rewriter
-                    .create<gpu::SubgroupMmaLoadMatrixOp>(
-                        loc, mmaTypeC, matC, ValueRange{zero, zero}, ldc,
-                        /*transpose=*/UnitAttr())
-                    .getRes();
+  auto tileC = rewriter.create<xegpu::InitTileOp>(
+      loc, tileTypeC, matC, ValueRange{}, ValueRange{}, ValueRange{},
+      SmallVector<int64_t>{0, 0}, tileTypeC.getShape(),
+      SmallVector<int64_t>{1, 1});
+  auto vecTypeC =
+      VectorType::get(tileTypeC.getShape(), tileTypeC.getElementType());
+
+  // No operands need transposition for now. Just present for API to be happy.
+  auto transpose = BoolAttr::get(rewriter.getContext(), false);
+
+  auto vnniAxisC = IntegerAttr::get(rewriter.getI32Type(), 0);
+  Value loadC = rewriter.create<xegpu::Load2DOp>(loc, vecTypeC, tileC,
+                                                 vnniAxisC, transpose);
 
   scf::ForOp batchLoop;
   Value batchIv;
@@ -181,34 +128,77 @@ static LogicalResult gemmToGpuMMA(linalg::LinalgOp linalgOp,
     Value batch =
         rewriter.create<arith::ConstantIndexOp>(loc, typeA.getShape()[0]);
     batchLoop =
-        rewriter.create<scf::ForOp>(loc, zero, batch, one, ValueRange{tileC});
+        rewriter.create<scf::ForOp>(loc, zero, batch, one, ValueRange{loadC});
     rewriter.setInsertionPointToStart(batchLoop.getBody());
     batchIv = batchLoop.getInductionVar();
-    tileC = batchLoop.getRegionIterArg(0);
+    loadC = batchLoop.getRegionIterArg(0);
   }
 
-  Value tileA = rewriter
-                    .create<gpu::SubgroupMmaLoadMatrixOp>(
-                        loc, mmaTypeA, matA,
-                        isBrgemm ? ValueRange{batchIv, zero, zero}
-                                 : ValueRange{zero, zero},
-                        lda,
-                        /*transpose=*/UnitAttr())
-                    .getRes();
-  Value tileB = rewriter
-                    .create<gpu::SubgroupMmaLoadMatrixOp>(
-                        loc, mmaTypeB, matB,
-                        isBrgemm ? ValueRange{batchIv, zero, zero}
-                                 : ValueRange{zero, zero},
-                        ldb, /*transpose=*/UnitAttr())
-                    .getRes();
+  SmallVector<Value> inputStrides{one, one};
+  if (isBrgemm)
+    inputStrides.push_back(one);
 
-  Value result =
-      rewriter
-          .create<gpu::SubgroupMmaComputeOp>(loc, tileC.getType(), tileA, tileB,
-                                             tileC, /*a_transpose=*/UnitAttr(),
-                                             /*b_transpose=*/UnitAttr())
-          .getRes();
+  // In case of brgemm, the batch offset is dynamic.
+  SmallVector<int64_t> staticOffsets;
+  if (isBrgemm)
+    staticOffsets.push_back(ShapedType::kDynamic);
+  staticOffsets.push_back(0);
+  staticOffsets.push_back(0);
+
+  // Always assume unit strides for loading. If a buffer is strided, the info
+  // will be encoded in the input memref type directly e.g., memref subview.
+  SmallVector<int64_t> staticStrides{1, 1};
+  if (isBrgemm)
+    staticStrides.push_back(1);
+
+  // Loaded tile is rank reduced in the batch dimension in case of brgemm.
+  SmallVector<int64_t> staticSizesA;
+  if (isBrgemm)
+    staticSizesA.push_back(1);
+  staticSizesA.push_back(tileTypeA.getShape()[0]);
+  staticSizesA.push_back(tileTypeA.getShape()[1]);
+  SmallVector<int64_t> staticSizesB;
+  if (isBrgemm)
+    staticSizesB.push_back(1);
+  staticSizesB.push_back(tileTypeB.getShape()[0]);
+  staticSizesB.push_back(tileTypeB.getShape()[1]);
+
+  constexpr int vnniFactor = 2;
+
+  auto tileA = rewriter.create<xegpu::InitTileOp>(
+      loc, tileTypeA, matA, isBrgemm ? ValueRange{batchIv} : ValueRange{},
+      ValueRange{}, ValueRange{}, staticOffsets, staticSizesA, staticStrides);
+  auto shapeA = tileTypeA.getShape();
+  auto vecTypeA =
+      VectorType::get({shapeA[0], shapeA[1] / vnniFactor, vnniFactor},
+                      tileTypeA.getElementType());
+
+  auto tileB = rewriter.create<xegpu::InitTileOp>(
+      loc, tileTypeB, matB, isBrgemm ? ValueRange{batchIv} : ValueRange{},
+      ValueRange{}, ValueRange{}, staticOffsets, staticSizesB, staticStrides);
+  auto shapeB = tileTypeB.getShape();
+  auto vecTypeB =
+      VectorType::get({shapeB[0] / vnniFactor, shapeB[1], vnniFactor},
+                      tileTypeB.getElementType());
+
+  auto vnniAxisA = IntegerAttr::get(rewriter.getI32Type(), 0);
+  auto vnniAxisB = IntegerAttr::get(rewriter.getI32Type(), 1);
+
+  auto loadA = rewriter.create<xegpu::Load2DOp>(loc, vecTypeA, tileA, vnniAxisA,
+                                                transpose);
+  auto loadB = rewriter.create<xegpu::Load2DOp>(loc, vecTypeB, tileB, vnniAxisB,
+                                                transpose);
+
+  // No loadC operand for accumulation?
+  auto dpas = rewriter.create<xegpu::DpasOp>(loc, vecTypeC, loadA, loadB);
+
+  // DPAS does not have accumulation operand so insert a eltwise vector add
+  // manually.
+  Value result;
+  if (llvm::isa<FloatType>(tileTypeC.getElementType()))
+    result = rewriter.create<arith::AddFOp>(loc, dpas, loadC);
+  else
+    result = rewriter.create<arith::AddIOp>(loc, dpas, loadC);
 
   if (isBrgemm) {
     rewriter.setInsertionPointToEnd(batchLoop.getBody());
@@ -218,121 +208,18 @@ static LogicalResult gemmToGpuMMA(linalg::LinalgOp linalgOp,
   }
 
   // Write back the total sum to the output buffer.
-  rewriter.create<gpu::SubgroupMmaStoreMatrixOp>(
-      loc, result, matC, ValueRange{zero, zero}, ldc, /*transpose=*/UnitAttr());
+  rewriter.create<xegpu::Store2DOp>(loc, tileC, result);
 
   rewriter.eraseOp(linalgOp);
 
   return success();
 }
 
-// Create loops out of matmul-like operation.
-static LogicalResult gemmToGpuLoops(linalg::LinalgOp linalgOp,
-                                    PatternRewriter &rewriter) {
-  assert((isa_and_nonnull<linalg::MatmulOp>(linalgOp) ||
-          isa_and_nonnull<linalg::BatchReduceMatmulOp>(linalgOp)) &&
-         "Requires a matmul like op for loop lowering");
-
-  Location loc = linalgOp.getLoc();
-
-  auto matA = linalgOp.getDpsInputOperands()[0]->get();
-  auto matB = linalgOp.getDpsInputOperands()[1]->get();
-  auto matC = linalgOp.getDpsInitOperands()[0]->get();
-
-  ArrayRef<int64_t> shapeC = matC.getType().cast<ShapedType>().getShape();
-  ArrayRef<int64_t> shapeA = matA.getType().cast<ShapedType>().getShape();
-
-  // Parallel dims.
-  Value i = rewriter.create<arith::ConstantIndexOp>(loc, shapeC[0]);
-  Value j = rewriter.create<arith::ConstantIndexOp>(loc, shapeC[1]);
-  // Reduction dim.
-  Value k = rewriter.create<arith::ConstantIndexOp>(loc, shapeA.back());
-  // Lbs.
-  Value zero = rewriter.create<arith::ConstantIndexOp>(loc, 0);
-  // Step.
-  Value one = rewriter.create<arith::ConstantIndexOp>(loc, 1);
-  SmallVector<Value> ivs;
-
-  // Create parallel loops over the outer dimensions.
-  auto parallelLoop = rewriter.create<scf::ParallelOp>(
-      loc, ValueRange{zero, zero}, ValueRange{i, j}, ValueRange{one, one});
-  auto parallelIvs = parallelLoop.getInductionVars();
-  ivs.append(parallelIvs.begin(), parallelIvs.end());
-
-  OpBuilder::InsertionGuard guard(rewriter);
-  rewriter.setInsertionPoint(parallelLoop.getBody()->getTerminator());
-
-  // Fetch the inital value of the output element.
-  Value initVal =
-      rewriter.create<memref::LoadOp>(loc, matC, parallelIvs).getResult();
-
-  bool isBrgemm = isa<linalg::BatchReduceMatmulOp>(linalgOp);
-  scf::ForOp batchLoop;
-  Value batchIv;
-  if (isBrgemm) {
-    Value batch = rewriter.create<arith::ConstantIndexOp>(loc, shapeA[0]);
-    batchLoop =
-        rewriter.create<scf::ForOp>(loc, zero, batch, one, ValueRange{initVal});
-    rewriter.setInsertionPointToStart(batchLoop.getBody());
-    batchIv = batchLoop.getInductionVar();
-    initVal = batchLoop.getRegionIterArg(0);
-  }
-
-  // Compute matmul with a loop over reduction dimension.
-  // Each GPU thread computes a single result element.
-  // Accumulate result locally through loop's iter args.
-  // This maps to more efficient computation as the accumulation is kept
-  // locally by a thread.
-  auto bodyBuilder = [&](OpBuilder &b, Location loc, Value localIv,
-                         ValueRange iterArgs) {
-    SmallVector<Value> loopIvs = ivs;
-    loopIvs.push_back(localIv);
-    assert(loopIvs.size() == 3);
-    Value localI = loopIvs[0];
-    Value localJ = loopIvs[1];
-    Value localK = loopIvs[2];
-    Value scalarA =
-        b.create<memref::LoadOp>(loc, matA,
-                                 isBrgemm ? ValueRange{batchIv, localI, localK}
-                                          : ValueRange{localI, localK});
-    Value scalarB =
-        b.create<memref::LoadOp>(loc, matB,
-                                 isBrgemm ? ValueRange{batchIv, localK, localJ}
-                                          : ValueRange{localK, localJ});
-    Value scalarMul = b.create<arith::MulFOp>(loc, scalarA, scalarB);
-    auto scalarAdd = b.create<arith::AddFOp>(loc, iterArgs[0], scalarMul);
-
-    b.create<scf::YieldOp>(loc, scalarAdd.getResult());
-  };
-  auto accumulationLoop = rewriter.create<scf::ForOp>(
-      loc, zero, k, one, ValueRange{initVal},
-      [&](OpBuilder &b, Location loc, Value iv, ValueRange iterArgs) {
-        bodyBuilder(b, loc, iv, iterArgs);
-      });
-
-  Value result = accumulationLoop.getResults()[0];
-
-  if (isBrgemm) {
-    rewriter.setInsertionPointToEnd(batchLoop.getBody());
-    rewriter.create<scf::YieldOp>(loc, ValueRange{result});
-    result = batchLoop.getResults()[0];
-    rewriter.setInsertionPointAfter(batchLoop);
-  }
-
-  // Write back the total sum to the output buffer.
-  rewriter.create<memref::StoreOp>(loc, result, matC, parallelIvs);
-
-  rewriter.eraseOp(linalgOp);
-
-  return success();
-}
-
-// Convert linalg.matmul to GPU-compatible kernel.
+// Convert linalg.matmul to XeGPU kernel.
 struct ConvertGemmToXegpu : public OpRewritePattern<linalg::MatmulOp> {
   using OpRewritePattern<linalg::MatmulOp>::OpRewritePattern;
 
-  ConvertGemmToXegpu(MLIRContext *ctx, bool useWmma)
-      : OpRewritePattern(ctx), useWmma(useWmma) {}
+  ConvertGemmToXegpu(MLIRContext *ctx) : OpRewritePattern(ctx) {}
 
   LogicalResult matchAndRewrite(linalg::MatmulOp matmulOp,
                                 PatternRewriter &rewriter) const override {
@@ -345,23 +232,18 @@ struct ConvertGemmToXegpu : public OpRewritePattern<linalg::MatmulOp> {
           matmulOp, "Expect static shape when mapping to GPU");
     }
 
-    if (useWmma && supportsMMACompute(matmulOp))
-      return gemmToGpuMMA(matmulOp, rewriter);
-    else
-      return gemmToGpuLoops(matmulOp, rewriter);
+    // TODO: add check if linalg op shapes and types are supported by XeGPU
+    // (like in WMMA).
+    return gemmToXegpu(matmulOp, rewriter);
   }
-
-private:
-  bool useWmma;
 };
 
-// Convert linalg.batch_reduce_matmul to GPU-compatible kernel.
+// Convert linalg.batch_reduce_matmul to XeGPU kernel.
 struct ConvertBrgemmToXegpu
     : public OpRewritePattern<linalg::BatchReduceMatmulOp> {
   using OpRewritePattern<linalg::BatchReduceMatmulOp>::OpRewritePattern;
 
-  ConvertBrgemmToXegpu(MLIRContext *ctx, bool useWmma)
-      : OpRewritePattern(ctx), useWmma(useWmma) {}
+  ConvertBrgemmToXegpu(MLIRContext *ctx) : OpRewritePattern(ctx) {}
 
   LogicalResult matchAndRewrite(linalg::BatchReduceMatmulOp brgemmOp,
                                 PatternRewriter &rewriter) const override {
@@ -374,14 +256,10 @@ struct ConvertBrgemmToXegpu
           brgemmOp, "Expect static shape when mapping to GPU");
     }
 
-    if (useWmma && supportsMMACompute(brgemmOp))
-      return gemmToGpuMMA(brgemmOp, rewriter);
-    else
-      return gemmToGpuLoops(brgemmOp, rewriter);
+    // TODO: add check if linalg op shapes and types are supported by XeGPU
+    // (like in WMMA).
+    return gemmToXegpu(brgemmOp, rewriter);
   }
-
-private:
-  bool useWmma;
 };
 
 void populateLinalgToXegpuPatterns(RewritePatternSet &patterns) {
