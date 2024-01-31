b33666315550aab9564e39039ebb8914bceb01a1
Lorenzo Chelini <l.chelini@icloud.com>
2024-01-31 13:10:35 +0100

    Trim includes in `CheckOps.h` (NFC)

--- include/TPP/Dialect/Check/CheckOps.h
+++ include/TPP/Dialect/Check/CheckOps.h
@@ -9,11 +9,9 @@
 #ifndef TPP_DIALECT_CHECK_CHECKOPS_H
 #define TPP_DIALECT_CHECK_CHECKOPS_H
 
-#include "mlir/Bytecode/BytecodeOpInterface.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
-#include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #define GET_OP_CLASSES
 #include "TPP/Dialect/Check/CheckOps.h.inc"
