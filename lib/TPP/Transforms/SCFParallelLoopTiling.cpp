acccd2f831cd31a56572ff727b6a562c1ae7d783
Kavitha Madhu <kavitha.madhu@intel.com>
2024-02-20 01:21:34 -0800

    Fixed comment

--- lib/TPP/Transforms/SCFParallelLoopTiling.cpp
+++ lib/TPP/Transforms/SCFParallelLoopTiling.cpp
@@ -1,4 +1,4 @@
-//===- ParallelLoopTiling.cpp - Tiles scf.parallel ------------------------===//
+//===- SCFParallelLoopTiling.cpp - Tiles scf.parallel ---------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
