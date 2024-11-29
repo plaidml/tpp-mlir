ce9c2c0b8da20776de7d26a37b3cbec741e9c562
Arun Thangamani <arun.thangamani@intel.com>
2024-11-29 01:41:38 -0800

    code refactoring: added new functions to check vector read op, subviews, and loop nest

--- lib/TPP/Transforms/HoistVectorTransfers.cpp
+++ lib/TPP/Transforms/HoistVectorTransfers.cpp
@@ -28,11 +28,78 @@ namespace tpp {
 } // namespace tpp
 } // namespace mlir
 
-using namespace mlir;
-using namespace vector;
-
 namespace mlir {
 namespace tpp {
+
+static FailureOr<SmallVector<vector::TransferReadOp>>
+getContractOperands(vector::ContractionOp contractOp) {
+  SmallVector<vector::TransferReadOp> list;
+  for (int i = 0; i < 3; i++) {
+    auto vectorReadOp =
+        contractOp.getOperand(i).getDefiningOp<vector::TransferReadOp>();
+    if (!vectorReadOp)
+      return failure();
+    list.push_back(vectorReadOp);
+  }
+  return list;
+}
+
+static FailureOr<SmallVector<memref::SubViewOp>>
+getReadOperands(SmallVector<vector::TransferReadOp> readOp) {
+  SmallVector<memref::SubViewOp> list;
+  for (int i = 0; i < 3; i++) {
+    auto subViewOp = readOp[i].getOperand(0).getDefiningOp<memref::SubViewOp>();
+    if (!subViewOp)
+      return failure();
+    list.push_back(subViewOp);
+  }
+  return list;
+}
+
+static FailureOr<SmallVector<scf::ForOp>>
+getNestedLoop(vector::ContractionOp contractOp,
+              SmallVector<memref::SubViewOp> subviews) {
+
+  auto subviewOpLhsOffsets = subviews[0].getOffsets();
+  auto subviewOpRhsOffsets = subviews[1].getOffsets();
+  auto subviewOpAccOffsets = subviews[2].getOffsets();
+
+  SmallVector<scf::ForOp> list;
+  scf::ForOp current = contractOp->getParentOfType<scf::ForOp>();
+  if (!current)
+    return failure();
+
+  // check the induction variable usage in subviews of vector read op
+  Value ivK = current.getInductionVar();
+  if (ivK != subviewOpLhsOffsets[2] || ivK != subviewOpRhsOffsets[1])
+    return failure();
+
+  list.push_back(current);
+  for (int i = 0; i < 3; i++) {
+    scf::ForOp parent = current->getParentOfType<scf::ForOp>();
+    if (!parent)
+      return failure();
+    list.push_back(parent);
+    current = parent;
+  }
+
+  // Check the induction variable usgae in subviews of vector read op for other
+  // loops
+  Value ivReduction = list[1].getInductionVar();
+  if (ivReduction != subviewOpLhsOffsets[0] ||
+      ivReduction != subviewOpRhsOffsets[0])
+    return failure();
+
+  Value ivN = list[2].getInductionVar();
+  if (ivN != subviewOpAccOffsets[1] || ivN != subviewOpRhsOffsets[2])
+    return failure();
+
+  Value ivM = list[3].getInductionVar();
+  if (ivM != subviewOpLhsOffsets[1] || ivM != subviewOpAccOffsets[0])
+    return failure();
+
+  return list;
+}
 
 struct HoistVectorTransferOp : OpRewritePattern<vector::ContractionOp> {
   using OpRewritePattern<vector::ContractionOp>::OpRewritePattern;
@@ -41,26 +108,23 @@ struct HoistVectorTransferOp : OpRewritePattern<vector::ContractionOp> {
                                 PatternRewriter &rewriter) const override {
 
     // Check the vector contract operation satisfies the required pattern.
-    // Check the ACC, LHS, and RHS of contract operation
-    auto vectorReadOpAcc =
-        contractOp.getAcc().getDefiningOp<mlir::vector::TransferReadOp>();
-    if (!vectorReadOpAcc)
-      return failure();
+    // Check the Acc, Lhs, and Rhs of contract operation
 
-    auto subviewOp =
-        vectorReadOpAcc.getOperand(0).getDefiningOp<memref::SubViewOp>();
-    if (!subviewOp)
-      return failure();
+    auto operands = getContractOperands(contractOp);
+    if (failed(operands))
+      return rewriter.notifyMatchFailure(contractOp,
+                                         "Invalid operands for contract op");
 
-    auto vectorReadOpLHS =
-        contractOp.getLhs().getDefiningOp<mlir::vector::TransferReadOp>();
-    if (!vectorReadOpLHS)
-      return failure();
+    auto readOp = *operands;
+    auto vectorReadOpAcc = readOp[2];
+    auto vectorReadOpLhs = readOp[0];
+    auto vectorReadOpRhs = readOp[1];
 
-    auto vectorReadOpRHS =
-        contractOp.getRhs().getDefiningOp<mlir::vector::TransferReadOp>();
-    if (!vectorReadOpRHS)
-      return failure();
+    // Check whether the operand of vector transfer read is a subview
+    auto subviews = getReadOperands(readOp);
+    if (failed(subviews))
+      return rewriter.notifyMatchFailure(
+          contractOp, "Vector read op operand(s) is/are not a subview");
 
     // Check the operation type MatMul, B-MatMul, or BR-MatMul
     auto contractIteratorTypes = contractOp.getIteratorTypesArray();
@@ -70,12 +134,12 @@ struct HoistVectorTransferOp : OpRewritePattern<vector::ContractionOp> {
         reductionCount++;
     }
 
-    auto vectorReadOpLHSType = cast<ShapedType>(vectorReadOpLHS.getType());
-    auto vectorReadOpRHSRank =
-        (cast<ShapedType>(vectorReadOpRHS.getType())).getRank();
+    auto vectorReadOpLhsType = cast<ShapedType>(vectorReadOpLhs.getType());
+    auto vectorReadOpRhsRank =
+        (cast<ShapedType>(vectorReadOpRhs.getType())).getRank();
 
     if (reductionCount == 2 &&
-        (vectorReadOpLHSType.getRank() != 3 || vectorReadOpRHSRank != 3))
+        (vectorReadOpLhsType.getRank() != 3 || vectorReadOpRhsRank != 3))
       return failure();
 
     if (reductionCount == 1)
@@ -88,56 +152,44 @@ struct HoistVectorTransferOp : OpRewritePattern<vector::ContractionOp> {
 
     // Check the K-dim to be 1
     int64_t K =
-        vectorReadOpLHSType.getDimSize(vectorReadOpLHSType.getRank() - 1);
+        vectorReadOpLhsType.getDimSize(vectorReadOpLhsType.getRank() - 1);
     if (K != 1)
       return rewriter.notifyMatchFailure(contractOp, "K dim is not 1");
 
     // Check whether the linalg tiling + vector contract pattern matches for the
     // 4-nested loop structure
-    auto oldKForOp = contractOp->getParentOfType<scf::ForOp>();
-    if (!oldKForOp)
+    auto loops = getNestedLoop(contractOp, *subviews);
+    if (failed(loops))
       return rewriter.notifyMatchFailure(
-          contractOp, "Not a linalg tile + vector contract operation");
+          contractOp, "Invalid loop nest in contract pattern");
 
-    auto oldReductionForOp = oldKForOp->getParentOfType<scf::ForOp>();
-    if (!oldReductionForOp)
-      return rewriter.notifyMatchFailure(
-          contractOp, "Not a linalg tile + vector contract operation");
-
-    auto oldNForOp = oldReductionForOp->getParentOfType<scf::ForOp>();
-    if (!oldNForOp)
-      return rewriter.notifyMatchFailure(
-          contractOp, "Not a linalg tile + vector contract operation");
-
-    auto oldMForOp = oldNForOp->getParentOfType<scf::ForOp>();
-    if (!oldMForOp)
-      return rewriter.notifyMatchFailure(
-          contractOp, "Not a linalg tile + vector contract operation");
+    auto nestedLoops = *loops;
+    auto kForOp = nestedLoops[0];
+    auto reductionForOp = nestedLoops[1];
 
     // Move the vector transfer read before the reduction and k loop
-    rewriter.setInsertionPoint(oldReductionForOp);
+    rewriter.setInsertionPoint(reductionForOp);
     auto *cloneVectorReadOp = rewriter.clone(*vectorReadOpAcc);
 
     // Code to re-create the reduction and k loop with iter args
-    auto vectorReadOpValue = (*cloneVectorReadOp).getResult(0);
+    auto vectorReadOpValue = cloneVectorReadOp->getResult(0);
     auto newReductionForOp = rewriter.create<scf::ForOp>(
-        oldReductionForOp.getLoc(), oldReductionForOp.getLowerBound(),
-        oldReductionForOp.getUpperBound(), oldReductionForOp.getStep(),
+        reductionForOp.getLoc(), reductionForOp.getLowerBound(),
+        reductionForOp.getUpperBound(), reductionForOp.getStep(),
         ValueRange{vectorReadOpValue},
         [&](OpBuilder &rewriterNewReductionForOp, Location locNewReductionForOp,
             Value ivNewReductionForOp, ValueRange iterArgsNewReductionForOp) {
           auto newKForOp = rewriter.create<scf::ForOp>(
-              oldKForOp.getLoc(), oldKForOp.getLowerBound(),
-              oldKForOp.getUpperBound(), oldKForOp.getStep(),
-              iterArgsNewReductionForOp,
+              kForOp.getLoc(), kForOp.getLowerBound(), kForOp.getUpperBound(),
+              kForOp.getStep(), iterArgsNewReductionForOp,
               [&](OpBuilder &rewriterNewKForOp, Location locNewKForOp,
                   Value ivNewKForOp, ValueRange iterArgsNewKForOp) {
-                mlir::IRMapping mapper;
-                mapper.map(oldReductionForOp.getInductionVar(),
+                IRMapping mapper;
+                mapper.map(reductionForOp.getInductionVar(),
                            ivNewReductionForOp);
-                mapper.map(oldKForOp.getInductionVar(), ivNewKForOp);
+                mapper.map(kForOp.getInductionVar(), ivNewKForOp);
 
-                for (auto &op : oldKForOp.getBody()->without_terminator()) {
+                for (auto &op : kForOp.getBody()->without_terminator()) {
                   rewriterNewKForOp.clone(op, mapper);
                 }
                 rewriterNewKForOp.create<scf::YieldOp>(locNewKForOp,
@@ -150,30 +202,28 @@ struct HoistVectorTransferOp : OpRewritePattern<vector::ContractionOp> {
     // Code to hoist vector transfer write after reduction loop and also to
     // update the yield of k loop
     auto newKForOp =
-        llvm::dyn_cast<mlir::scf::ForOp>(newReductionForOp.getBody()->front());
+        llvm::dyn_cast<scf::ForOp>(newReductionForOp.getBody()->front());
     Value newcontractOpValue;
-    mlir::vector::TransferWriteOp vectorWriteOperation;
-    mlir::Block *bodyBlock = newKForOp.getBody();
+    vector::TransferWriteOp vectorWriteOperation;
+    Block *bodyBlock = newKForOp.getBody();
     for (auto &op : bodyBlock->getOperations()) {
-      if (auto vectorContractOp =
-              llvm::dyn_cast<mlir::vector::ContractionOp>(op)) {
+      if (auto vectorContractOp = llvm::dyn_cast<vector::ContractionOp>(op)) {
         vectorContractOp.setOperand(vectorContractOp.getNumOperands() - 1,
                                     newKForOp.getRegionIterArgs()[0]);
         newcontractOpValue = vectorContractOp.getResult();
       }
-      if (auto yieldOp = llvm::dyn_cast<mlir::scf::YieldOp>(op)) {
+      if (auto yieldOp = llvm::dyn_cast<scf::YieldOp>(op)) {
         if (newcontractOpValue != NULL)
           yieldOp.setOperand(0, newcontractOpValue);
       }
-      if (auto vectorWriteOp =
-              llvm::dyn_cast<mlir::vector::TransferWriteOp>(op)) {
+      if (auto vectorWriteOp = llvm::dyn_cast<vector::TransferWriteOp>(op)) {
         vectorWriteOperation = vectorWriteOp;
       }
     }
 
     if (vectorWriteOperation != NULL) {
       vectorWriteOperation.setOperand(0, newReductionForOp.getResult(0));
-      vectorWriteOperation->moveBefore(oldReductionForOp);
+      vectorWriteOperation->moveBefore(reductionForOp);
     }
 
     // Erase the old vector contract operation
